d7656cc36fed2cfa02cae37f37a35fadf51b83e1
Asus <Asus@LAPTOP-915NAFGL>
2025-10-03 18:21:11 +0530

    third commit and push

--- Sep_Selenium/src/sep/Student_1.java
+++ Sep_Selenium/src/sep/Student_1.java
@@ -7,10 +7,17 @@ public class Student_1 {
 		System.out.println("Welcome to all of you");
 	}
 	
+	public void display2()
+	{
+		System.out.println("Automation is very easy");
+	}
+	
+	
 	public static void main(String[] args) {
 		
 		Student_1 s1=new Student_1();
 		s1.display1();
+		s1.display2();
 		
 	}
 
