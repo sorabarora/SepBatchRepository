47b60e0031732359f4b0d4611e0d7356cea51200
Asus <Asus@LAPTOP-915NAFGL>
2025-10-03 18:16:41 +0530

    second commit and push

---
+++ Sep_Selenium/src/sep/Student_1.java
@@ -0,0 +1,17 @@
+package sep;
+
+public class Student_1 {
+	
+	public void display1()
+	{
+		System.out.println("Welcome to all of you");
+	}
+	
+	public static void main(String[] args) {
+		
+		Student_1 s1=new Student_1();
+		s1.display1();
+		
+	}
+
+}
